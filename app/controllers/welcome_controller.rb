6e2f905766a042aba5b2fe20ecf14863f1d5b376
Jose Antonio <ajose9975@gmail.com>
2022-07-26 14:12:02 -0300

    Setting Layouts & Backoffices

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  # http_basic_authenticate_with name: "jose", password: "jose"
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
   devise_for :users
   devise_for :admins
-  get 'welcome/index'
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
 end
