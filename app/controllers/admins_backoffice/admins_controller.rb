d5dd5eada2afa412ca44e72663a770050b0de23e
Jose Antonio <ajose9975@gmail.com>
2022-07-27 18:22:31 -0300

    Add Index, edit and update to admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,4 +1,24 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
+    @admins = Admin.all
+  end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+
+    @admin = Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+    if @admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Admin atualizado com sucesso."
+    else
+      render :edit
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
   namespace :site do
     get 'welcome/index'
   end
+
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
   namespace :admins_backoffice do
     get 'welcome/index'
+    resources :admins, only: [:index, :edit, :update]
   end
+
   devise_for :users
   devise_for :admins
+
+  get 'inicio', to: 'site/welcome#index'
+
   root to: 'site/welcome#index'
 end
