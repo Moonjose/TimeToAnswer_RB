ab0d88e43cfbfa668cbdd209160c9cc655a995a3
Jose Antonio <ajose9975@gmail.com>
2022-07-28 13:52:47 -0300

    Refactoring controller, new admin and flash msg

--- app/assets/javascripts/admins_backoffice.coffee
+++ app/assets/javascripts/admins_backoffice.coffee
@@ -5,5 +5,5 @@
 //= require bootstrap/dist/js/bootstrap
 //= require metismenu/dist/metisMenu
 //= require sb-admin-2
-
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit, :update]
+    resources :admins, except: [:delete]
   end
 
   devise_for :users
