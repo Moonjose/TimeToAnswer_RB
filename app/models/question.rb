e1b984bfbb75df13f23313d7c0ef620520310801
Jose <ajose9975@gmail.com>
2022-08-02 17:19:28 -0300

    Filter question by subject

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,13 +8,22 @@ class Question < ApplicationRecord
    paginates_per 5
 
    # Scopes
+
+   scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
    scope :_search_, ->(page, term){
-     includes(:answers)
-     .where("lower(description) LIKE ?", "%#{term.downcase}%")
-     .page(page)
+    includes(:answers, :subject)
+    .where("lower(description) LIKE ?", "%#{term.downcase}%")
+    .page(page)
    }
  
    scope :last_questions, ->(page) {
-     includes(:answers).order('created_at desc').page(page)
+    includes(:answers, :subject)
+    .order('created_at asc')
+    .page(page)
    }
 end
