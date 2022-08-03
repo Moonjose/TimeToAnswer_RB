de554c347aed8a19b16679579de10240f4dee36b
Jose <ajose9975@gmail.com>
2022-08-03 16:10:08 -0300

    Add Avatar & Active Storage

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -9,7 +9,9 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   def update
     if @user.update(params_user)
       bypass_sign_in(@user)
-      redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
+      unless params_user[:user_profile_attributes][:avatar]
+        redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
+      end
     else
       render :edit
     end    
@@ -23,7 +25,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
   def params_user
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-      user_profile_attributes: [:id, :address, :gender, :birthdate])
+    user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
   end
 
   def verify_password
